ac1f232c52b7cefc2b00c20c245c4033388c3609
Paul-543NA <paul.festor@gmail.com>
2024-07-01 15:08:52 +0100

    Fixed theme

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" data-theme="dark">
+    <html lang="en">
       {/* <body className={inter.className}> */}
       {/* {runtimeParams.singleViewMode ? (
             <IntervalBar lower={30} actual={50} upper={90} />
