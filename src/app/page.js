1ef737c43c0595fea093028a7a487193108f3a54
Paul-543NA <paul.festor@gmail.com>
2024-07-19 12:48:11 +0100

    Buki edit

--- src/app/page.js
+++ src/app/page.js
@@ -21,14 +21,12 @@ const Page = () => {
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content text-center">
         <div className="max-w-lg">
-          <h1 className="text-5xl font-bold dark:text-gray-300">
-            Hello there!
-          </h1>
+          <h1 className="text-5xl font-bold dark:text-gray-300">Hello Buki!</h1>
           <p className="py-8 text-xl">
-            You are about to take part in a study of human AI collaboration. We
-            are very excited to count you as one of our participants! It will
-            only take a few minutes and if you do well you might even win a gift
-            card!
+            You are about to take part in a study of human AI collaboration (we
+            know you like that). We are very excited to count you as one of our
+            participants! It will only take a few minutes and if you do well you
+            might even win a gift card!
           </p>
           <button className="btn btn-primary text-lg" onClick={handleClick}>
             {didCompleteForm ? "Continue" : "Get started"}
