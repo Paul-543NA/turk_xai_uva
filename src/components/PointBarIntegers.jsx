9dbbe9e8dac5b9f0484f1e9bd82641f8e27b35e2
tabearoeber <t.e.rober@uva.nl>
2024-10-04 13:55:41 +0200

    Update IntervalBarIntegers.jsx and PointBarIntegers.jsx

--- src/components/PointBarIntegers.jsx
+++ src/components/PointBarIntegers.jsx
@@ -82,7 +82,7 @@ const PointBarForIntegers = ({
       <div className="relative w-full flex justify-between">
         {integerValues.map((value) => (
           <NormalCircle
-            // key={value}
+            key={value}
             value={value}
             position={getPosition(value)}
             isActual={value === actual}
