725c47f36e19dfe43490d0dff833380466a73936
tabearoeber <t.e.rober@uva.nl>
2025-03-31 15:25:20 +0200

    Update page.js
    Add message that this study is closed

--- src/app/page.js
+++ src/app/page.js
@@ -17,6 +17,35 @@ const Page = () => {
     }
   };
 
+  // return (
+  //   <div className="hero bg-base-200 min-h-screen">
+  //     <div className="hero-content text-center">
+  //       <div className="max-w-lg">
+  //         <h1 className="text-5xl font-bold dark:text-gray-300">
+  //           Hello there!
+  //         </h1>
+  //         <p className="py-8 text-xl">
+  //           You are about to take part in a study of human AI collaboration. We
+  //           are very excited to count you as one of our participants! It will
+  //           take about 30 minutes and if you do well you might even win a gift
+  //           card!
+  //         </p>
+  //         <button className="btn btn-primary text-lg" onClick={handleClick}>
+  //           {didCompleteForm ? "Continue" : "Get started"}
+  //         </button>
+  //       </div>
+  //     </div>
+  //     <div className="fixed bottom-4 w-full flex justify-center">
+  //       <p className="text-center text-sm text-gray-500 max-w-6xl px-4">
+  //         To receive financial compensation for participating in this study, you
+  //         must complete the survey fully, sensibly, and truthfully. We have
+  //         quality criteria in place to assess this. If these criteria are not met,
+  //         you will not receive credit for participation (via Amazon Mechanical Turk) and will not be eligible
+  //         to win a gift card.
+  //       </p>
+  //     </div>
+  //   </div>
+  // );
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content text-center">
@@ -25,24 +54,18 @@ const Page = () => {
             Hello there!
           </h1>
           <p className="py-8 text-xl">
-            You are about to take part in a study of human AI collaboration. We
-            are very excited to count you as one of our participants! It will
-            take about 30 minutes and if you do well you might even win a gift
-            card!
+            This study has been terminated. Thank you for your interest.
           </p>
-          <button className="btn btn-primary text-lg" onClick={handleClick}>
-            {didCompleteForm ? "Continue" : "Get started"}
-          </button>
         </div>
       </div>
       <div className="fixed bottom-4 w-full flex justify-center">
         <p className="text-center text-sm text-gray-500 max-w-6xl px-4">
-          To receive financial compensation for participating in this study, you
-          must complete the survey fully, sensibly, and truthfully. We have
-          quality criteria in place to assess this. If these criteria are not met,
-          you will not receive credit for participation (via Amazon Mechanical Turk) and will not be eligible
-          to win a gift card.
-        </p>
+           To receive financial compensation for participating in this study, you
+           must complete the survey fully, sensibly, and truthfully. We have
+           quality criteria in place to assess this. If these criteria are not met,
+           you will not receive credit for participation (via Amazon Mechanical Turk) and will not be eligible
+           to win a gift card.
+         </p>
       </div>
     </div>
   );
